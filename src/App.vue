6efc98d8d4b1bab739dec1e3c02e48cee4adc59e
lihao <lihao.coder@foxmail.com>
2023-03-28 22:39:27 +0800

    init li-ui

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Lihao from "./components/Lihao.vue";
+</script>
+
+<template>
+  <div>hi</div>
+  <Lihao />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
