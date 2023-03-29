205b1c7bf74f473be45393d09a334aa5ba652252
lihao <lihao.coder@foxmail.com>
2023-03-30 00:37:50 +0800

    完成 Switch 组件

--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import SwitchUi from "../lib/SwitchUi.vue";
+const bool = ref(false);
+</script>
 <template>
-  <div>Switch</div>
+  <div class="switch">
+    <p>size: large | disabled: false</p>
+    <SwitchUi v-model:value="bool" disabled size="large" />
+  </div>
+  <div class="switch">
+    <p>size: default | disabled: true</p>
+    <SwitchUi :value="bool" @update:value="bool = $event" />
+  </div>
+  <div class="small">
+    <p>size: large | disabled: false</p>
+    <SwitchUi v-model:value="bool" disabled size="small" />
+  </div>
 </template>
+<style lang="scss" scoped></style>
---
+++ src/lib/SwitchUi.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import { defineProps, defineEmits, readonly } from "vue";
+const props = defineProps({
+  value: {
+    type: Boolean,
+    required: true,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  size: {
+    type: String,
+    default: "default",
+    validator(value: string) {
+      return ["large", "default", "small"].includes(value);
+    },
+  },
+});
+const emits = defineEmits(["update:value"]);
+
+const switchSize = readonly({
+  large: "14px",
+  default: "18px",
+  small: "24px",
+});
+
+const toggle = () => {
+  emits("update:value", !props.value);
+};
+</script>
+<template>
+  <button
+    :disabled="props.disabled"
+    @click="toggle"
+    :class="{ checked: props.value, disabled: props.disabled }"
+  >
+    <!-- <span></span> -->
+  </button>
+</template>
+
+<style lang="scss" scoped>
+$h: v-bind("switchSize[props.size]");
+$h2: calc(#{$h} - 4px);
+button {
+  height: $h;
+  width: calc(#{$h} * 2);
+  border: none;
+  background: grey;
+  border-radius: calc(#{$h} / 2);
+  position: relative;
+  cursor: pointer;
+  &:focus {
+    outline: none;
+  }
+  &.checked {
+    background: blue;
+    &::after {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+  &.disabled {
+    cursor: not-allowed;
+    opacity: 0.4;
+  }
+  &::after {
+    content: "";
+    display: inline-block;
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: calc(#{$h2} / 2);
+    transition: left 250ms;
+  }
+}
+</style>
