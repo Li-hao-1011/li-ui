7acf7b37f51488df9400f34f737247082706a699
lihao <lihao.coder@foxmail.com>
2023-03-29 14:17:54 +0800

    完成Home的css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,14 +5,36 @@ import TopNav from "../components/TopNav.vue";
 <template>
   <TopNav />
   <div class="banner">
-    <h1>li-ui</h1>
+    <h1>LI-UI</h1>
     <h2>一个基于Vue3的UI组件库</h2>
     <p class="actions">
-      <a href="">GitHub</a>
+      <a href="https://github.com/Li-hao-1011">GitHub</a>
       |
-      <a href="">开始</a>
+      <RouterLink to="/doc">开始</RouterLink>
     </p>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
+</style>
