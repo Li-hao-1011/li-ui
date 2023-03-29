29886ec5765b3051fedbcf61111ad9afcdd4aa2b
lihao <lihao.coder@foxmail.com>
2023-03-29 14:28:29 +0800

    调节 aside 和 TopNav的位置

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -4,8 +4,39 @@ import TopNav from "../components/TopNav.vue";
 
 <template>
   <TopNav />
-  <div class="banner">
-    <aside>边栏</aside>
-    <mian>主体</mian>
+  <div class="content">
+    <aside>
+      <h2>组件列表</h2>
+      <ol>
+        <li><RouterLink to="/doc/switch">Switch 组件</RouterLink></li>
+        <li><RouterLink to="/doc/button">Button 组件</RouterLink></li>
+        <li><RouterLink to="/doc/dialog">Dialog 组件</RouterLink></li>
+        <li><RouterLink to="/doc/Tabs">Tabs 组件</RouterLink></li>
+      </ol>
+    </aside>
+    <mian>
+      <RouterView />
+    </mian>
   </div>
 </template>
+<style lang="scss" scoped>
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
+  z-index: 1;
+
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
+</style>
