56ba05227a186e034e2e3f055f40776acc11733e
lihao <lihao.coder@foxmail.com>
2023-03-30 22:02:17 +0800

    Button 添加样式

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
 import ButtonUi from "../lib/ButtonUi.vue";
+const onCLick = () => {
+  console.log("你好");
+};
+const onFocus = () => {
+  console.log("onFocus");
+};
+const onMouse = () => {
+  console.log("onMouse");
+};
 </script>
 <template>
   <div>
     <h1>Button</h1>
-    <ButtonUi>你好</ButtonUi>
+    <ButtonUi>你好 </ButtonUi>
+    <br />
+    <ButtonUi theme="button">你好 </ButtonUi>
+    <br />
+    <ButtonUi theme="link">你好 </ButtonUi>
+    <br />
+    <ButtonUi theme="text">你好 </ButtonUi>
   </div>
 </template>
