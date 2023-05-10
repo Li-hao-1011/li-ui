47c9b202df8e75a8e187b0b13e901928edfeeaa0
lihao <lihao.coder@foxmail.com>
2023-05-10 09:09:34 +0800

    创建 lib/index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+export { default as Button } from "./ButtonUi.vue";
+export { default as Dialog } from "./DialogUi.vue";
+export { default as Switch } from "./SwitchUi.vue";
+export { default as Tabs } from "./TabsUi.vue";
+export { default as Tab } from "./TabUi.vue";
+export { openDialog as openDialog } from "./openDialog";
